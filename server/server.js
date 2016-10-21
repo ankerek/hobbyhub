c0958d3b4ace6395e45ac2db6d1c10d5e6a518af
ankerek <ankerek@gmail.com>
2016-10-21 23:06:39 +0200

    add morgan logger

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import path from 'path';
+import morgan from 'morgan';
 import config from '../configs';
 import routes from './routes';
 
 const app = express();
 
-app.use(routes);
-
+// server index.html file in production
 if(config.env === 'production') {
   const publicPath = '/';
   const outputPath = path.resolve(process.cwd(), 'static');
@@ -16,11 +16,9 @@ if(config.env === 'production') {
   app.get('*', (req, res) => res.sendFile(path.resolve(outputPath, 'index.html')));
 }
 
+app.use(morgan('tiny'));
+app.use(routes);
+
 app.listen(config.port, function() {
   console.log(`Server started: http://${config.host}:${config.port}/`);
 });
-
-// webpack dev server for hot reloading
-// if(config.env === 'development') {
-//   require('./webpack-dev-server');
-// }
