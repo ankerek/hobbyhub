fad92faef49674a0c2e524419f7bb7241a2eb8cd
michalbric <michalbric@gmail.com>
2016-10-27 11:04:29 +0200

    add basic mongoose models

--- server/models/event.js
+++ server/models/event.js
@@ -1,7 +1,93 @@
 import mongoose from 'mongoose';
 
+// reused for organiser and individual attendees
+const attendeeSchema = new mognoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  averageRating: {
+    type: Number,
+    required: false,
+    min: 0, max: 100,
+  },
+  state: {
+    type: String,
+    enum: ['PENDING', 'ACCEPTED'], default: 'PENDING',
+    required: true,
+  },
+});
+
+// recursive comment schema
+const commentSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  authoerName: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  replies: [commentSchema],
+});
+
+
+// the actual event schema
 const EventSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+  },
+  organizer: {
+    type: attendeeSchema,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  categories: {
+    type: [CategorySchema],
+    default: [],
+    required: true,
+  },
+  start: {
+    type: Date,
+    required: true,
+  },
+  end: {
+    type: Date,
+    required: true,
+  },
+  minPeople: {
+    type: Number,
+    required: true,
+    min: 2
+  },
+  maxPeople: {
+    type: Number,
+    required: true,
+    min: 2,
+  },
+  attendees: [attendeeSchema],
+  comments: [commentSchema],
 });
 
 export default mongoose.model('Event', EventSchema, 'events');
---
+++ server/models/user.js
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose';
+
+// rating sub-document schema
+const ratingSchema = new mongoose.Schema({
+  ratedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true , //TODO: refs for backend, autoincrement id for api?
+  },
+  event: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  eventName: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  percent: {
+    type: Number,
+    required: true,
+    min: 0, max: 100,
+  },
+  additionalText: {
+    type: String,
+    required: false,
+  },
+});
+
+// the user document schema
+const UserSchema = new mongoose.Shema({
+  email: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  introducation: {
+    type: String,
+    required: false,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  ratings: [ratingSchema],
+});
+
+export default mongoose.model('User', UserSchema, 'users');
