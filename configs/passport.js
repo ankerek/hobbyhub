d9be79631c4ff21995e29e3f0ab399a339b6e274
Ondřej Bína <binao@seznam.cz>
2016-11-12 19:17:19 +0100

    add basic local authentication

---
+++ configs/passport.js
@@ -0,0 +1,51 @@
+import {Strategy as LocalStrategy} from 'passport-local';
+import User from '../server/models/user';
+import passport from 'passport';
+
+passport.use('local-signup', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, (req, email, password, done) => {
+  process.nextTick(() => {
+    User.findOne({
+      'email': email
+    }, (err, user) => {
+      if (err) {
+        return done(err)
+      };
+      if (user) {
+        return done(null, false, {message: 'That email is already taken.'});
+      } else {
+        const newUser = new User(req.body);
+        newUser.password = User.generateHash(password);
+        newUser.save((err) => {
+          if (err) {
+            throw err;
+          }
+          return done(null, newUser);
+        });
+      }
+    });
+  });
+}));
+
+passport.use('local-signin', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, (req, email, password, done) => {
+  User.findOne({
+    'email': email
+  }, (err, user) => {
+    if (err) 
+      return done(err);
+    if (!user) 
+      return done(null, false, {message: 'No user found.'});
+    if (!user.validPassword(password)) 
+      return done(null, false, {message: 'Oops! Wrong password.'});
+    return done(null, user);
+  });
+}));
+
+export default passport;
--- server/controllers/users_controller.js
+++ server/controllers/users_controller.js
@@ -1,33 +1,60 @@
-import User from '../models/user'
+import User from '../models/user';
+import passport from '../../configs/passport';
 
 export function index(req, res, next) {
-  User.find((err,users) => {
-          if(err) {
-            return next(err);
-          }
-        res.json(users);
-    })
+  User.find((err, users) => {
+    if (err) {
+      return next(err);
+    }
+    res.json(users);
+  })
 }
 
 export function create(req, res, next) {
-  const user = new User(req.body);
-    user.save((err, user) => {
-        if (err) {
-          return next(err);
-        }
-        res.json(user);
-    });
+  passport.authenticate('local-signup', {
+    session: false
+  }, (err, user, info) => {
+    if (err) {
+      res
+        .status(500)
+        .json(err);
+    } else if (info) {
+      res
+        .status(401)
+        .json(info);
+    } else {
+      res
+        .status(200)
+        .json(user);
+    }
+  })(req, res, next);
 }
 
 export function show(req, res, next) {
   User.findById(req.params.userId, (err, user) => {
-        if (err) {
-          return next(err);
-        }
-        res.json(user);
-      })
+    if (err) {
+      return next(err);
+    }
+    res.json(user);
+  })
 }
 
-export function auth(req, rest, next) {
-  //TODO: passport, hardcode? whatever
+export function auth(req, res, next) {
+  passport.authenticate('local-signin', {
+    session: false
+  }, (err, user, info) => {
+    if (err) {
+      res
+        .status(500)
+        .json(err);
+    } else if (info) {
+      res
+        .status(401)
+        .json(info);
+    } else {
+      res
+        .status(200)
+        .json(user);
+    }
+  })(req, res, next);
 }
