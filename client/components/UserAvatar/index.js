3b0ad9837a23c281b8bdd4c68fef38374ce9b726
Lukáš Mladý <mladylukas@gmail.com>
2016-11-13 19:12:05 +0100

    chore(static): move avatars

--- client/components/UserAvatar/index.js
+++ client/components/UserAvatar/index.js
@@ -2,24 +2,17 @@ import React from 'react';
 
 import './index.scss';
 
-import anonymousAvatars from '../../constants/anonymousAvatars';
-import { bm, } from '../../utils/bem';
-
-const randomGenerator = (min, max) => Math.floor(Math.random() * max) + min;
+import { bm } from '../../utils/bem';
 
 export const renderUserAvatar = ({
   moduleName = 'UserAvatar',
   user,
   size = 32,
-} = {}) => {
-  const avatarSrc = anonymousAvatars[randomGenerator(0, anonymousAvatars.length - 1)];
-
-  return (
-    <div className={bm(moduleName)}>
-      <img src={avatarSrc} alt={user.fullName} width={size} height={size} />
-    </div>
-  );
-};
+} = {}) => (
+  <div className={bm(moduleName)}>
+    <img src={user.pictureUrl} alt={user.fullName} width={size} height={size} />
+  </div>
+);
 
 const UserAvatar = (renderUserAvatar);
 
