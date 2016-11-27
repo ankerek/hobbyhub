78aebedb0b678ace7a50bf781c2dafee5d238a34
ankerek <ankerek@gmail.com>
2016-11-27 20:28:20 +0100

    format event time

--- client/screens/EventFormScreen.js
+++ client/screens/EventFormScreen.js
@@ -6,6 +6,12 @@ import { fetchEvent, createEvent } from '../actions/events';
 import { getEvent } from '../reducers/entities';
 import EventForm from '../components/EventForm';
 
+const formatEventTime = (event) => {
+  event.start = event.start.slice(0, -1);
+  event.end = event.end.slice(0, -1);
+  return event;
+}
+
 const mapStateToProps = (state, { params: { id } }) => ({
   categories: getAllCategoriesNames(state),
   id,
@@ -17,30 +23,19 @@ const mapDispatchToProps = {
   createEvent,
 };
 
-
 class EventFormScreenContainer extends React.Component {
   componentDidMount() {
     const { id, event, fetchEvent } = this.props;
-    if(id) fetchEvent(id);
+    if(id && !event._id) fetchEvent(id);
   }
 
   render() {
     const { event, categories, createEvent } = this.props;
     const isEdit = event && event._id;
-    return <EventForm onSubmit={createEvent} categories={categories} initialValues={isEdit ? event : null} />
+
+    return <EventForm onSubmit={createEvent} categories={categories} initialValues={isEdit ? formatEventTime(event) : null} />
   }
 }
-
-// export const EventFormScreenView = ({
-//   categories,
-//   handleCreateEvent,
-// }) => (
-//   <EventForm onSubmit={handleCreateEvent} categories={categories} />
-// );
-
-// EventFormScreenView.propTypes = {
-//   dispatch: T.func.isRequired,
-// };
 
 const EventFormScreen = compose(
   connect(mapStateToProps, mapDispatchToProps),
