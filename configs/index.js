3aa232d1e7da6bf9e7f2a4eb8a29211c4db75a15
Ondřej Bína <binao@seznam.cz>
2017-01-20 21:33:24 +0100

    add sample config

--- configs/index.js
+++ configs/index.js
@@ -9,4 +9,6 @@ module.exports = Object.assign({
   host: process.env.HOST || 'localhost',
   port: process.env.PORT || 3000,
   mongodb: process.env.MONGOLAB_URI,
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY
 }, myconfig);
